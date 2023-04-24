c0abd7eb192b75d0c8b91f1dd6f5d8fe3fbf5c66
lanhhtran <tranlananhpt2k3@gmail.com>
2023-04-24 22:35:40 +0700

    + add the exercises of lab1

---
+++ week1/src/week1/b1.java
@@ -0,0 +1,9 @@
+package week1;
+import java.util.Scanner;
+public class b1 {
+public static void main(String []args) {
+	Scanner scanner = new Scanner(System.in);
+	System.out.println("Nhap vao so nguyen n: ");
+	int n = scanner.nextInt();
+}
+}
