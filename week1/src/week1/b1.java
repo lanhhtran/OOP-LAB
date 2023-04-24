999ada71865eda6366bc5d3875d941df45d07880
lanhhtran <tranlananhpt2k3@gmail.com>
2023-04-24 22:49:26 +0700

    ~ change lab1

--- week1/src/week1/b1.java
+++
@@ -1,9 +0,0 @@
-package week1;
-import java.util.Scanner;
-public class b1 {
-public static void main(String []args) {
-	Scanner scanner = new Scanner(System.in);
-	System.out.println("Nhap vao so nguyen n: ");
-	int n = scanner.nextInt();
-}
-}
